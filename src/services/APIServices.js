899f01b9eb4cc1ec01fa7963728c8974d631a882
kbiganashvili <k.biganashvili.websmart@gmail.com>
2022-09-05 14:41:03 +0400

    finished modal, fixed some code

--- src/services/APIServices.js
+++ src/services/APIServices.js
@@ -29,15 +29,8 @@ export const withToken = async (method, url, token, id) => {
 };
 
 export const postMethod = (body) => {
-  for (const pair of body.entries()) {
-    console.log(`${pair[0]}, ${pair[1]}`);
-  }
-
   Axios.post('https://pcfy.redberryinternship.ge/api/laptop/create', body)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  .catch((error) => {
+    console.log(error);
+  });
 };
